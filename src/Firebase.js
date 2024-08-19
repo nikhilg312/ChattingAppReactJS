a83432d4cfd2b726dc9e2a2a945853459dd7b027
nikhilg312 <79538782+nikhilg312@users.noreply.github.com>
2024-08-20 00:06:45 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import {getStorage} from "firebase/storage"
 import {getFirestore} from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyAHaOCCbPIAV9fOdYghT_-PDpTGA_ZiaS0",
+  apiKey: "",
   authDomain: "chat-capstone.firebaseapp.com",
   projectId: "chat-capstone",
   storageBucket: "chat-capstone.appspot.com",
-  messagingSenderId: "707636660214",
-  appId: "1:707636660214:web:ee3fb0ec165fa9706529d7"
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
